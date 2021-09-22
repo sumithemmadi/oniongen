45a4c280312d6a93cbac76d6c7f1bb09bffa27bc
sumithemmadi <sumithemmadi244@gmail.com>
2021-09-22 15:31:14 +0530

    Updated

---
+++ test.go
@@ -0,0 +1,122 @@
+/*
+MIT License
+
+Copyright (c) 2021 Emmadi Sumith Kumar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package main
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/sha512"
+	"encoding/base32"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+//        "time"
+        "math/rand"
+	"golang.org/x/crypto/sha3"
+)
+//var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+/*
+var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+
+
+func RandomVariable(n int) string {
+    b := make([]rune, n)
+    for i := range b {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
+}
+func main() {
+    rand.Seed(time.Now().UnixNano())
+
+    fmt.Println(RandomVariable(1))
+}
+*/
+func generate() {
+		publicKey, secretKey, err := ed25519.GenerateKey(nil)
+		checkErr(err)
+		onionAddress := encodePublicKey(publicKey)
+	  fmt.Println(onionAddress+".onion")
+	  save(onionAddress, publicKey, expandSecretKey(secretKey))
+}
+
+func expandSecretKey(secretKey ed25519.PrivateKey) [64]byte {
+
+	hash := sha512.Sum512(secretKey[:32])
+	hash[0] &= 248
+	hash[31] &= 127
+	hash[31] |= 64
+	return hash
+
+}
+
+func encodePublicKey(publicKey ed25519.PublicKey) string {
+
+	// checksum = H(".onion checksum" || pubkey || version)
+	var checksumBytes bytes.Buffer
+	checksumBytes.Write([]byte(".onion checksum"))
+	checksumBytes.Write([]byte(publicKey))
+	checksumBytes.Write([]byte{0x03})
+	checksum := sha3.Sum256(checksumBytes.Bytes())
+
+	// onion_address = base32(pubkey || checksum || version)
+	var onionAddressBytes bytes.Buffer
+	onionAddressBytes.Write([]byte(publicKey))
+	onionAddressBytes.Write([]byte(checksum[:2]))
+	onionAddressBytes.Write([]byte{0x03})
+	onionAddress := base32.StdEncoding.EncodeToString(onionAddressBytes.Bytes())
+
+	return strings.ToLower(onionAddress)
+
+}
+
+func save(onionAddress string, publicKey ed25519.PublicKey, secretKey [64]byte) {
+	os.MkdirAll("RandomAddress/"+onionAddress, 0700)
+
+	secretKeyFile := append([]byte("== ed25519v1-secret: type0 ==\x00\x00\x00"), secretKey[:]...)
+	checkErr(ioutil.WriteFile("RandomAddress/"+onionAddress+"/hs_ed25519_secret_key", secretKeyFile, 0600))
+
+	publicKeyFile := append([]byte("== ed25519v1-public: type0 ==\x00\x00\x00"), publicKey...)
+	checkErr(ioutil.WriteFile("RandomAddress/"+onionAddress+"/hs_ed25519_public_key", publicKeyFile, 0600))
+
+	checkErr(ioutil.WriteFile("RandomAddress/"+onionAddress+"/hostname", []byte(onionAddress+".onion"), 0600))
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	// Set runtime to use all available CPUs.
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	go generate()
+}
